9928996511eb621c9b260b5b5a38a01dee3840fc
SagarMakhar <sagarmakhar@gmail.com>
2022-01-08 18:49:07 +0000

    vendor: theme: Black theme [3/3]
    Change-Id: I362c75a9a9456bb4d7ffe12072b1b2ef1ea49dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -84,7 +84,7 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
+# LEAVE LESS INformation available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
 # Charger
@@ -224,7 +224,8 @@ PRODUCT_PACKAGES += \
 
 # NexusLauncher resources
 PRODUCT_PACKAGES += \
-    NexusLauncherResOverlay
+    NexusLauncherResOverlay \
+    BlackThemeOverlay
 
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
