6655dbc33623cb8d6c64cbb59d23904eb6f5be1a
electimon <electimon@gmail.com>
2022-06-18 19:30:24 +0000

    vendor: qcom: Include qcom_boards.mk
    * Can you believe it was missed?
    
    Change-Id: I6e535638ffde11ffd96c86f5b9208e68046efd6e
    Signed-off-by: electimon <electimon@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -10,6 +10,8 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
+include $(TOPDIR)vendor/aosp/build/core/vendor/qcom_boards.mk
+
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
