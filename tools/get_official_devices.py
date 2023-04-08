f94223b24b4292e5900d28efad412693901c158c
Yohan Black <103707560+Yohan-Black@users.noreply.github.com>
2023-04-07 23:01:57 -0500

    SkylineUI: Adapt Vendor to our Project

--- tools/get_official_devices.py
+++ tools/get_official_devices.py
@@ -14,12 +14,12 @@ except:
     urllib.parse = urlparse
     urllib.request = urllib2
 try:
-    url = "https://raw.githubusercontent.com/PixelExperience/official_devices/master/devices.json"
+    url = "https://raw.githubusercontent.com/soon/weekly_updates_ota/master/devices.json"
     response = urllib.request.urlopen(url, timeout=10)
     data = json.loads(response.read())
     for res in data:
         for version in res['supported_versions']:
-            if version['version_code'] == 'thirteen_plus':
+            if version['version_code'] == 'aosp-13':
                 print (res['codename'])
                 break
 except:
