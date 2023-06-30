3f3e36a98b3f3875b07ea445428d7838f206ba2b
Joey Huab <joey@evolution-x.org>
2023-07-01 01:34:16 +0900

    SkylineUI: Add Changelog
    - Thanks EvolutionX for the base of this script
    
    Co-Authored-By: David Ramírez  <DavidYonatan9@hotmail.com>

---
+++ build/tools/changelog.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Exports
+DEVICE=$(echo $TARGET_DEVICE | sed 's/skylineui_//g')
+out=$OUT_DIR/target/product/$DEVICE
+if [ -f $out ]; # The path does not exist
+then
+	echo "The path to create changelog does not exist, exiting..."
+	exit
+fi
+
+export Changelog=Changelog.txt
+
+if [ -f $Changelog ];
+then
+	rm -f $Changelog
+fi
+
+# Assuming this script is run from TOP of workspace
+export formatter_script=$(realpath vendor/aosp/build/tools/changelog_repo_formatter.sh)
+
+touch $Changelog
+
+# Print something to build output
+echo "Generating changelog..."
+
+for i in $(seq 14);
+do
+export After_Date=$(/usr/bin/date --date="$i days ago" +%Y/%m/%d)
+k=$(expr $i - 1)
+	export Until_Date=$(/usr/bin/date --date="$k days ago" +%Y/%m/%d)
+
+	# Line with after --- until was too long for a small ListView
+	echo '=======================' >> $Changelog;
+	echo  "     "$Until_Date       >> $Changelog;
+	echo '=======================' >> $Changelog;
+	echo >> $Changelog;
+
+	# Cycle through every repo to find commits between 2 dates
+	repo forall -c '. $formatter_script' >> $Changelog
+	echo >> $Changelog;
+done
+
+cp $Changelog $OUT_DIR/target/product/$DEVICE/
