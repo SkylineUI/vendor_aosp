9f52002eda22dc3756d7505505fd1be375b5ebef
Joey Huab <joey@evolution-x.org>
2023-07-01 01:34:16 +0900

    SkylineUI: Add Changelog
    - Thanks EvolutionX for the base of this script
    
    Co-Authored-By: David Ramírez  <DavidYonatan9@hotmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -25,6 +25,8 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) $(SHA256) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).sha256sum
 	$(hide) $(MD5) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/aosp/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(CUSTOM_VERSION).zip $(SKYLINEUI_BASE_VERSION)
+	$(hide) ./vendor/aosp/build/tools/changelog.sh 
+	$(hide) mv Changelog.txt $(CUSTOM_TARGET_PACKAGE).txt
 	@echo -e ${CL_CYN}""${CL_CYN}
 	@echo -e ${CL_CYN}"   _____ _          _ _            _    _ _____ "${CL_CYN}
 	@echo -e ${CL_CYN}"  / ____| |        | |_|          | |  | |_   _|"${CL_CYN}
